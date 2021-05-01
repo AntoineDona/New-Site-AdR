6b9b953f20cc772009a86db6619d3fe6c9fb20bd
Antoine Do Nascimento <antoinedn@live.fr>
2021-05-01 04:35:28 +0200

    database maj

--- Musée/Gestion/database.php
+++ Musée/Gestion/database.php
@@ -1,16 +1,1 @@
-<?php
-//config SQL
-$DBhost  = "localhost";
-$DBowner = "root";
-$DBpw    = $_ENV["db_password"];
-$DBName  = "adr";
-
-$DBconnect = "mysql:dbname=".$DBName.";host=".$DBhost;
-
-// $pdo = new PDO($DBconnect, $DBowner, $DBpw);
-// $bdd = new PDO('mysql:host=localhost;dbname=adr;charset=utf8', 'root', 'Morangis91');
-
-$pdo = new PDO('mysql:host=localhost;port=3307;dbname=adr;charset=utf8', 'root', 'root');
-$bdd = new PDO('mysql:host=localhost;port=3307;dbname=adr;charset=utf8', 'root', 'root');
-
-?>
+<?php include("../included/database.php"); ?>
