5391f94e056732629c645a4a05853f49fa33394b
Antoine Do Nascimento <antoinedn@live.fr>
2021-05-01 13:46:09 +0200

    css page musée

--- musee/included/header.php
+++ musee/included/header.php
@@ -1,38 +1,38 @@
 <header>
     <nav>
         <div class="logo">
-            <a href="/Musée/index.php"><img src="/Musée/img/logo_musee_b.png" alt=""></a>
+            <a href="/musee/index.php"><img src="/musee/img/logo_musee_b.png" alt=""></a>
         </div>
         <h1>Le Musée</h1>
         <ul class="nav-links">
             <li class="nav-item">
-                <a href="/Musée/index.php" class="nav-link <?php if ($page == 'home') {
+                <a href="/musee/index.php" class="nav-link <?php if ($page == 'home') {
                                                                 echo "current";
                                                             } ?>">Accueil</a>
             </li>
             <li class="nav-item">
-                <a href="/Musée/Menu" class="nav-link <?php if ($page == 'menu') {
+                <a href="/musee/Menu" class="nav-link <?php if ($page == 'menu') {
                                                             echo "current";
                                                         } ?>" target="_blank">Menu</a>
             </li>
             <li class="nav-item">
-                <a href="/Musée/commander.php" class="nav-link <?php if ($page == 'commander') {
+                <a href="/musee/commander.php" class="nav-link <?php if ($page == 'commander') {
                                                                     echo "current";
                                                                 } ?>">Commander</a>
             </li>
             <li class="nav-item">
-                <a href="/Musée/apropos.php" class="nav-link <?php if ($page == 'apropos') {
+                <a href="/musee/apropos.php" class="nav-link <?php if ($page == 'apropos') {
                                                                     echo "current";
                                                                 } ?>">A propos</a>
             </li>
             <li class="espace_adr">
-                <a href="/Musée/connexion.php" class="nav-link <?php if ($page == 'connexion') {
+                <a href="/musee/connexion.php" class="nav-link <?php if ($page == 'connexion') {
                                                                     echo "current";
                                                                 } ?>">Connexion</a>
             </li>
         </ul>
         <!-- <div class="espace_adr">
-                <a href="/Musée/espace_adr.php" class="nav-link">Connexion</a>
+                <a href="/musee/espace_adr.php" class="nav-link">Connexion</a>
             </div> -->
         <div class="burger">
             <div class="line1"></div>
