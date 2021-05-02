f9f261bcb33becc8b4652ed2fa47ada10e890608
Antoine Do Nascimento <antoinedn@live.fr>
2021-05-02 22:53:31 +0200

    bug icon medias

--- musee/connexion.php
+++ musee/connexion.php
@@ -20,9 +20,7 @@ if (isset($_SESSION['is_connected']) and $_SESSION['is_connected']) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include "/included/meta.php" ?>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="style.css">
+    <?php include "included/meta.php" ?>
     <title>Connexion</title>
 </head>
 
