97ba5e0ebd38dd16df409daff7527a0282c43054
Antoine Do Nascimento <antoinedn@live.fr>
2021-05-02 22:55:56 +0200

    bug auth icon

--- musee/connexion.php
+++ musee/connexion.php
@@ -20,6 +20,7 @@ if (isset($_SESSION['is_connected']) and $_SESSION['is_connected']) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
     <?php include "included/meta.php" ?>
     <title>Connexion</title>
 </head>
