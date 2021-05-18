2aa65482d2e2aba4d604678b1faca10957d46dd0
Antoine Do Nascimento <antoinedn@live.fr>
2021-05-18 17:24:09 +0200

    google verification

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <html lang="en">
 <head>
     <?php include "included/meta.php" ?>
-    <meta name="ADRCS ADR Centrale Supelec CentraleSupélec" content="AdR CentraleSupélec. L'Association des résidents de CentaleSupélec organise les grandes soirées de l'écoles CentraleSupélec, s'occupe des relations avec le gestionnaire de la résidence et tient le Musée, bar de l'école">
+    <meta name="description" content="AdR CentraleSupélec. L'Association des résidents de CentaleSupélec organise les grandes soirées de l'écoles CentraleSupélec, s'occupe des relations avec le gestionnaire de la résidence et tient le Musée, bar de l'école">
+    <meta name="google-site-verification" content="cEbrs-eyoHMLzEcQwiEu5sHkC8N61J92Z_fElR1KTMQ" />
     <title>AdR CentraleSupélec</title>
 </head>
 <body>
