39cce5897b7a6954386c52541ea27f0c53bdb721
Antoine Do Nascimento <antoinedn@live.fr>
2021-06-08 13:22:09 +0200

    ajout prix traite et en cours

--- musee/gestion/en_cours.php
+++ musee/gestion/en_cours.php
@@ -122,6 +122,9 @@ include("database.php"); ?>
 					<div style="display: flex; flex: 1; flex-flow:row wrap">
 						<?php display_cmd(unserialize($donnees['commande'])) ?>
 					</div>
+					<div style="margin:0.5rem;">
+						<strong>TOTAL: <?php echo $donnees['prix']; ?>€ </strong>
+					</div>
 				</div>
 				<?php if(!empty($donnees['commentaire'])){
 					?>
