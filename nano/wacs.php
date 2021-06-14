9930e422d44147aeffadc25c6f178d4237c142ff
Antoine Do Nascimento <antoinedn@live.fr>
2021-06-14 17:05:36 +0200

    test nano vieus site

---
+++ nano/wacs.php
@@ -0,0 +1,30 @@
+<?php session_start();
+echo "unauthorized";
+$_SESSION["joke"]=1;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+<title>WACS</title>
+    </head>
+<div class="jeu">
+  <h1> TROUVE LE BON LIEN FDP DE WACS </h1>
+  <a href="#">lien numéro 1</a>
+  <a href="#">lien numéro 1</a>
+  <a href="https://youporn.com">lien numéro 1</a>
+  <a href="https://youjizz.com">lien numéro 1</a>
+  <a href="https://hqporner.com">lien numéro 1</a>
+  <a href="https://wacs.cs-campus.fr">lien numéro 1</a>
+  <a href="https://9gag.com">lien numéro 1</a>
+  <a href="https://fr.wikipedia.org/wiki/ski">lien numéro 1</a>
+  <a href="https://facebook.fr">lien numéro 1</a>
+  <a href="https://youtube.fr">lien numéro 1</a>
+  <a href="https://legorafi.fr">lien numéro 1</a>
+  <a href="#">lien numéro 1</a>
+  <a href="#">lien numéro 1</a>
+  <a href="https://www.ikea.com/fr/fr/p/klaemmig-serviette-a-capuche-gris-jaune-60373186/">lien numéro 1</a>
+  <a href="#">lien numéro 1</a>
+  <a href="#">lien numéro 1</a>
+  <a href="#">lien numéro 1</a>
+</div>
+</html>
