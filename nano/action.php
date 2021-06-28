c399450253667c368a04a74abce2e2afff19aa32
Antoine Do Nascimento <antoinedn@live.fr>
2021-06-28 20:06:58 +0200

    fix: use timestamp io manual seconds converter

--- nano/action.php
+++ nano/action.php
@@ -43,10 +43,10 @@ $_SESSION["promo"]=$_SESSION["user"]["promo"];
 date_default_timezone_set('Europe/Paris');
 $current_date_sec = (((date('d')-1)*24 + date('H'))*60 + date('i'))*60 + date('s');
 $shotgun_date = mktime(21, 0, 0, 9, 28, 2020);
-$shotgun_date_sec = (((date('d', $shotgun_date)-1)*24 + date('H', $shotgun_date))*60 + date('i', $shotgun_date))*60 + date('s', $shotgun_date);
+// $shotgun_date_sec = (((date('d', $shotgun_date)-1)*24 + date('H', $shotgun_date))*60 + date('i', $shotgun_date))*60 + date('s', $shotgun_date);
 $end_date = mktime(23, 50, 0, 9, 29, 2022);
-$end_date_sec = (((date('d', $end_date)-1)*24 + date('H', $end_date))*60 + date('i', $end_date))*60 + date('s', $end_date);
-if (time() >= $shotgun_date ) { //|| $current_date_sec <= $end_date_sec
+// $end_date_sec = (((date('d', $end_date)-1)*24 + date('H', $end_date))*60 + date('i', $end_date))*60 + date('s', $end_date);
+if (time() >= $shotgun_date && time() <= $end_date) { 
 	if ($_SESSION["cotisant"] == false) {
 		header("Location: https://adr.cs-campus.fr/nano/non_cotisant.php");
 	} else {
