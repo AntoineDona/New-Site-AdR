0ec1ea569d2a54181526c712c84bd1d88037a77a
Antoine Do Nascimento <antoinedn@live.fr>
2021-06-30 00:14:57 +0200

    feat: add photo des vieux + style

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ function showPanel(panelIndex){
     tabPanels.forEach(function(node){
         node.style.display="none";
     });
-    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.display="flex";
 }
 
-showPanel(1);
+showPanel(0);
 
 //transition de pages
 const swup = new Swup();
