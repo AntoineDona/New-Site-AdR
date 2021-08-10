8c8d436ac7287349d57690c78710ebd936cd0768
Antoine Do Nascimento <antoinedn@live.fr>
2021-08-11 00:00:24 +0200

    fix: wrong redirect url

--- nanolympique/redirect.php
+++ nanolympique/redirect.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$redirect_uri = "https://adr.cs-campus.fr/nanolympique/index.php";
+$redirect_uri = "https://adr.cs-campus.fr/nanolympique";
 $client_id = "47e7231e6e5c333459f9280e6d3c7eef96b38ce6";
 $client_secret = "0dee01e0437f6b07c784ae54af60b790d83fbcb0";
 $response_type = "code";
