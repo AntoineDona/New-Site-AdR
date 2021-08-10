938b86daa5b23643b638b1afb39e9de1bde25bd5
Antoine Do Nascimento <antoinedn@live.fr>
2021-08-10 11:10:46 +0200

    feat: deleting nano3

--- nano3/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-//config SQL
-$DBhost  = "localhost";
-$DBowner = "root";
-$DBpw    = "Morangis91";
-$DBName  = "adr";
-$DBconnect = "mysql:dbname=".$DBName.";host=".$DBhost;
-$pdo = new PDO($DBconnect, $DBowner, $DBpw);
-?>
--- nano3/get_date_milliseconds.php
+++
@@ -1,4 +0,0 @@
-<?php
-$date_milliseconds = intval(microtime(TRUE)*1000);
-echo $date_milliseconds;
-?>
