52b41c335771a5cbad1caef10cd325c1a456013d
Antoine Do Nascimento <antoinedn@live.fr>
2021-08-10 11:10:46 +0200

    feat: deleting nano3

--- nano3/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-//config SQL
-$DBhost  = "localhost";
-$DBowner = "root";
-$DBpw    = $_ENV["db_password"];
-$DBName  = "adr";
-$DBconnect = "mysql:dbname=".$DBName.";host=".$DBhost;
-$pdo = new PDO($DBconnect, $DBowner, $DBpw);
-?>
