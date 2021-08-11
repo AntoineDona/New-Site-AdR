c63f0c8dc33a68d46fabf5afb0d572de03ca0276
Antoine Do Nascimento <antoinedn@live.fr>
2021-08-11 22:52:05 +0200

    feat: Prettier code + test soldout

--- nanolympique/fini.php
+++ nanolympique/fini.php
@@ -8,40 +8,41 @@ session_start();
 
 <head>
 	<title>SOLD-OUT</title>
+	<link rel="shortcut icon" sizes="96x96" type="image/png" href="/images/favicon/favicon-96x96.png">
 	<link rel="stylesheet" href="styles.css">
 </head>
 
-<body class="finito">
-
 <?php
-function number_place($pdo){
-		$query = $pdo->prepare("SELECT COUNT(*) as c from nanolympique");
-		$query->execute();
-		$result= $query->fetch();
-		return $result['c'];
+function number_place($pdo)
+{
+	$query = $pdo->prepare("SELECT COUNT(*) as c from nanolympique");
+	$query->execute();
+	$result = $query->fetch();
+	return $result['c'];
 }
-if (number_place($pdo)<550){
-	if ($_SESSION["isConnected"]){
+if (number_place($pdo) < 0) {
+	if ($_SESSION["isConnected"]) {
 		header("Location: https://adr.cs-campus.fr/nanolympique/index.php");
-	}
-	else{
+	} else {
 		header("Location: https://adr.cs-campus.fr/nanolympique/connexion.php");
 	}
 }
-if ($_SESSION['shotgun']){
+if ($_SESSION['shotgun']) {
 	header("Location: https://adr.cs-campus.fr/nanolympique/index.php");
 }
 ?>
-	<div class="fin" style="background-color:rgba(255,255,255,0.9);width:70%;margin:auto;">
-	<h1 class="soldout">SOLD-OUT</h1>
-	<h1 class="finito">Le monde appartient à ceux qui se lèvent tôt</h1>
-	<h2 class="finito"><br>
-		<?php
-	
-	echo 'Salut '.$_SESSION["prenom"].', vérifie si tu as bien ta place en rechargeant !';
-	?>
-	
-	</h2>
+
+<body>
+	<div class="fin">
+		<h1 class="soldout">SOLD-OUT</h1>
+		<h1 class="finito">Le monde appartient à ceux qui se lèvent tôt! Et à ceux qui ont la fibre...</h1>
+		<h2 class="finito"><br>
+			<?php
+
+			echo 'Salut ' . $_SESSION["prenom"] . ', vérifie si tu as bien ta place en rechargeant !';
+			?>
+
+		</h2>
 	</div>
 	<div href="#" onClick="window.location='index.php'" id="reload">&#8635; Recharger</div>
 
