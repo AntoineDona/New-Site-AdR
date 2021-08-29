90524b4c4c1a5be5af796b3f48702fb011e45f81
Antoine Do Nascimento <antoinedn@live.fr>
2021-08-29 13:34:39 +0200

    feat: interface pour la soirée

--- nanolympique/interface.php
+++ nanolympique/interface.php
@@ -1,84 +1,80 @@
-<?php
-session_start();
-header("Location: https://adr.cs-campus.fr/nanolympique/interfacev2.php");
-?>
+<?php session_start(); ?>
 <?php include("database.php"); ?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
+
 	<title>Interface</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<link rel="shortcut icon" sizes="96x96" type="image/png" href="/images/favicon/favicon-96x96.png">
 	<link rel="stylesheet" href="styles.css">
 </head>
 
-<body class="Présence">
+<body class="presence">
 
-<?php
-function number_place($pdo){
-		$query = $pdo->prepare("SELECT COUNT(*) as c from nanoween");
+	<?php
+	function number_place($pdo)
+	{
+		$query = $pdo->prepare("SELECT COUNT(*) as c from nanolympique");
 		$query->execute();
-		$result= $query->fetch();
+		$result = $query->fetch();
 		return $result['c'];
-}
+	}
 
+	function entree($pdo)
+	{
+		$query = $pdo->prepare("SELECT COUNT(*) as d from nanolympique WHERE entree='oui'");
+		$query->execute();
+		$result = $query->fetch();
+		return $result['d'];
+	}
 
-?>
+	?>
 
-	<h1 class="interface">Vérifie si cette personne est déjà entrée ou non</h1>
-	<h1 class="finito">Cherche son prenom</h1>
-	<h2 class="finito">Valide UNIQUEMENT si la personne n'est pas déjà entrée.<br> 
-	
-	
-	
+	<?php $nombre = entree($pdo);
+	echo "<h1 class='nb_entrees'>Entrées: " . $nombre . "</h1>"; ?>
 
-<form action="#" method="post">
-<input style="font-size:1.5em;font-family:'CaviarDreams';" type="text" name="pseudo"> 
-<input style="font-size:1.5em;font-family:'CaviarDreams';" type="submit" name="Rechercher"> 
-</form> </h2>
-		
-		<?php 
-if(isset($_POST['Rechercher'])) // Si il clique sur valider
-{
-$_SESSION["search"] = $_POST['pseudo']; // Ici on va defenire que la session bla bla bla va être définie comme ce que il a écrit dans pseudo
+	<form id="searchform" action="#" method="post">
+		<input id="search" type="text" name="pseudo" placeholder="Chercher un prenom / nom / famille de parrainage" />
+		<button type="submit" id="submit" form="searchform" name="Rechercher" value=" ">
+			<i class="fas fa-search"></i>
+		</button>
+	</form>
 
-?><?php
-// Si tout va bien, on peut continuer
+	<?php
+	if (isset($_POST['Rechercher'])) { // Si il clique sur valider
+		$_SESSION["search"] = $_POST['pseudo']; // Ici on va defenire que la session bla bla bla va être définie comme ce que il a écrit dans pseudo
+		// Si tout va bien, on peut continuer
+		$reponse = $pdo->query("SELECT * FROM nanolympique WHERE CONCAT_WS('', prenom, nom, famille) LIKE '%" . $_SESSION["search"] . "%' ");
+		// On affiche chaque entrée une à une
+		while ($donnees = $reponse->fetch()) {
 
-$reponse = $pdo->query("SELECT * FROM nanoween WHERE prenom LIKE '".$_SESSION["search"]."%' ");
-?><?php
-// On affiche chaque entrée une à une
-while ($donnees = $reponse->fetch())
-{
-?>
-    <p class="liste">
-    <!--<strong class="nom">Nom</strong> : --><?php echo $donnees["prenom"]." ".$donnees["nom"]; ?><br /><br />
-    <!--Statut entrée : <br/>--><?php if ($donnees["entree"]=='non'){
-						echo "Pas encore entré(e)"; 
-					}
-	    				else{
-						echo "DÉJÀ ENTRÉ(E)";
-					}?><br>
-	    <form action="valide.php?prenom=<?php echo $donnees['prenom'];?>&nom=<?php echo $donnees['nom'];?>" method="POST" class="valide">
-     		<input type="submit" name="valider" value="Faire entrer">
-  		</form>
-		
-   </p>
+			if ($donnees["entree"] == 'non') {
+	?>
+				<div class="greencard">
+					<div class="nom">
+						Nom: <?php echo $donnees["prenom"] . ' ' . $donnees["nom"]; ?>
+					</div>
+					<div class="famille">
+						Famille: <?php echo $donnees["famille"] ?>
+					</div>
+					<form action="valide.php?prenom=<?php echo $donnees['prenom'] . '&nom=' . $donnees['nom'] ?>" method="POST" class="valide">
+						<input class="submit" type="submit" name="valider" value="Faire entrer">
+					</form>
+				</div>
+	<?php
+			} else {
+				echo '<div class="redcard">';
+				echo $donnees["prenom"] . ' ' . $donnees["nom"] . '<br><br>';
+				echo "<trong>DÉJÀ ENTRÉ(E)</strong>";
+				echo '</div>';
+			}
+		}
+		$reponse->closeCursor(); // Termine le traitement de la requête
+	}
+	?>
 
-	
-<?php
-}
-
-$reponse->closeCursor(); // Termine le traitement de la requête
-	
-}
-
-?>
-	
-	
-	</h2>
-	
-	<!-- <div href="#" onClick="window.location='index.php'" id="reload">&#8635; Recharger</div> -->
 
 </body>
 
