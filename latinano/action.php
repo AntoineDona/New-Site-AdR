a3408362b54646bca4604e97258e1ac9bc3c887e
Antoine Do Nascimento <antoinedn@live.fr>
2021-09-05 02:33:46 +0200

    feat: update action.php - nouvelle date sg - infos en + bdd

--- latinano/action.php
+++ latinano/action.php
@@ -49,6 +49,16 @@ function family_size($email, $pdo){
 
 $_SESSION["fsize"] = family_size($_SESSION["email"],$pdo);
 
+function family_name($email, $pdo){
+
+	$query = $pdo->prepare("SELECT * from representants_fp where email=?");
+	$query->execute(array($email));
+	$result= $query->fetch();
+	return $result['famille'];
+}
+
+$_SESSION["fname"] = family_name($_SESSION["email"],$pdo);
+
 // function isCotisant($mail,$pdo){
 // 	$query = $pdo->prepare("SELECT * from parrains WHERE EMAIL=?");
 // 	$query->execute(array($mail));
@@ -71,10 +81,10 @@ $_SESSION["promo"]=$_SESSION["user"]["promo"];
 date_default_timezone_set('Europe/Paris');
 $current_date_sec = (((date('d')-1)*24 + date('H'))*60 + date('i'))*60 + date('s');
 
-$shotgun_date = mktime(19, 0, 0, 8, 29, 2021);
+$shotgun_date = mktime(21, 0, 0, 9, 8, 2021);
 $shotgun_date_sec = (((date('d', $shotgun_date)-1)*24 + date('H', $shotgun_date))*60 + date('i', $shotgun_date))*60 + date('s', $shotgun_date);
 
-$end_date = mktime(22, 00, 0, 8, 29, 2021);
+$end_date = mktime(22, 00, 0, 9, 8, 2021);
 $end_date_sec = (((date('d', $end_date)-1)*24 + date('H', $end_date))*60 + date('i', $end_date))*60 + date('s', $end_date);
 if ($current_date_sec >= $shotgun_date_sec && $current_date_sec <= $end_date_sec) {
 	if ($_SESSION["is_representant"] == false) {
@@ -83,8 +93,8 @@ if ($current_date_sec >= $shotgun_date_sec && $current_date_sec <= $end_date_sec
 	} else {
 		if (!$_SESSION['shotgun']) {
 			if (number_place($pdo) + $_SESSION["fsize"] < 880) {
-				$query=$pdo->prepare("INSERT into latinano (prenom,nom, email, heure, taille) VALUES (?,?,?,?,?)");
-				$query->execute(array($_SESSION["prenom"],$_SESSION["nom"],$_SESSION["email"],$_SESSION['sg_time'],$_SESSION["fsize"]));
+				$query=$pdo->prepare("INSERT into latinano (prenom,nom, email, heure, taille, famille) VALUES (?,?,?,?,?,?)");
+				$query->execute(array($_SESSION["prenom"],$_SESSION["nom"],$_SESSION["email"],$_SESSION['sg_time'],$_SESSION["fsize"],$_SESSION["fname"]));
 				//$_SESSION['shotgun'] = true;
 				header("refresh:5; url=/latinano/index.php");
             } else {
