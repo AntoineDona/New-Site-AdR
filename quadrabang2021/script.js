607f4fd87136b11bcd4d1f845ca71f3a4bfa9161
Antoine Do Nascimento <antoinedn@live.fr>
2021-09-21 21:03:54 +0200

    fix: counter wouldn't work jquery conversion issu

--- quadrabang2021/script.js
+++ quadrabang2021/script.js
@@ -8,6 +8,7 @@ function onLoad() {
 	loading_page.style.opacity = '0';
 	loading_page.style.visibility = 'hidden';
 	parallax_group.style.overflowY = 'visible';
+
 	timer();
 };
 
@@ -20,7 +21,7 @@ function timer() {
 	var diff_min = getFormattedDigits(Math.trunc((total_sec - diff_jour * 24 * 3600 - diff_hour * 3600) / 60));
 	var diff_sec = getFormattedDigits(Math.trunc(total_sec - diff_jour * 24 * 3600 - diff_hour * 3600 - diff_min * 60));
 	if (total_sec >= 0) {
-		document.getElementById('#counter').html(
+		document.getElementById("counter").innerHTML =
 			'<div style="grid-area: d_top;">' + diff_jour + '</div>' +
 			'<div style="grid-area: h_top;">' + diff_hour + '</div>' +
 			'<div style="grid-area: m_top;">' + diff_min + '</div>' +
@@ -28,9 +29,9 @@ function timer() {
 			'<div style="grid-area: d_bottom; font-size:20%;">jour(s)</div>' +
 			'<div style="grid-area: h_bottom; font-size:20%;">heure(s)</div>' +
 			'<div style="grid-area: m_bottom; font-size:20%;">minute(s)</div>' +
-			'<div style="grid-area: s_bottom; font-size:20%;">seconde(s)</div>');
+			'<div style="grid-area: s_bottom; font-size:20%;">seconde(s)</div>';
 	} else {
-		document.getElementById('#counter').html('00:00:00:00');
+		document.getElementById('counter').innerHTML = '00:00:00:00';
 	}
 
 	setTimeout(timer, 1000);
