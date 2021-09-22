d5e5547c6b04ee4b191493ca3a5d66b969779419
Antoine Do Nascimento <antoinedn@live.fr>
2021-09-22 18:35:06 +0200

    feat: burger menu

--- quadrabang2021/script.js
+++ quadrabang2021/script.js
@@ -13,6 +13,7 @@ function onLoad() {
 		}, 2000)
 	}, 2000)
 	timer();
+	navSlide();
 };
 
 function timer() {
@@ -61,4 +62,25 @@ function timer() {
 		}
 		prevScrollpos = currentScrollPos;
 	}
+}
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',() => {
+        //toggle Nav
+        nav.classList.toggle('nav-active');
+        //Animate Links
+        navLinks.forEach((link,index) => {
+            if (link.style.animation){
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
 }
