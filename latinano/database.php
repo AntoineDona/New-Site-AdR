0177195a7c5d4d4a2a44462a427624b05fd42a94
Antoine Do Nascimento <antoinedn@live.fr>
2021-09-27 19:07:31 +0200

    fix: db problem

--- latinano/database.php
+++ latinano/database.php
@@ -6,8 +6,8 @@ $DBpw    = "Morangis91";
 $DBName  = "adr";
 $DBconnect = "mysql:dbname=".$DBName.";host=".$DBhost;
 
-$pdo = new PDO($DBconnect, $DBowner, $DBpw);
+// $pdo = new PDO($DBconnect, $DBowner, $DBpw);
 
-// $pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'root', 'root');
-// $bdd = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'root', 'root');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'root', 'root');
+$bdd = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'root', 'root');
 ?>
