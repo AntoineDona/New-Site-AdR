973a9120a024734f85b695ae941dbacd70eb7380
Antoine Do Nascimento <antoinedn@live.fr>
2021-11-11 20:25:47 +0100

    fix: change auth and db

--- nanovni/database.php
+++ nanovni/database.php
@@ -6,7 +6,7 @@ $DBpw    = $_ENV["db_password"];
 $DBName  = "adr";
 $DBconnect = "mysql:dbname=".$DBName.";host=".$DBhost;
 
-// $pdo = new PDO($DBconnect, $DBowner, $DBpw);
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'root', 'root');
+$pdo = new PDO($DBconnect, $DBowner, $DBpw);
+// $pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'root', 'root');
 
 ?>
