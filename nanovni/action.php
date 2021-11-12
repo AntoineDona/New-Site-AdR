cf3125878954e61d2366badb43e3e18dcbc75332
Antoine Do Nascimento <antoinedn@live.fr>
2021-11-12 01:18:59 +0100

    debug action.php

--- nanovni/action.php
+++ nanovni/action.php
@@ -46,23 +46,32 @@ $shotgun_date_sec = (((date('d', $shotgun_date)-1)*24 + date('H', $shotgun_date)
 
 $end_date = mktime(22, 00, 0, 11, 29, 2021);
 $end_date_sec = (((date('d', $end_date)-1)*24 + date('H', $end_date))*60 + date('i', $end_date))*60 + date('s', $end_date);
+echo "<p>On est en 0</p>";
 if ($current_date_sec >= $shotgun_date_sec && $current_date_sec <= $end_date_sec) {
+	echo "<p>On est en 1</p>";
 	if ($_SESSION["is_cotisant"] == false) {
+		echo "<p>On est en 2</p>";
 		$_SESSION['prev_page']="action.php";
 		header("Location: /nanovni/index.php");
 	} else {
+		echo "<p>On est en 3</p>";
 		if (!$_SESSION['shotgun']) {
+			echo "<p>On est en 4</p>";
 			if (number_place($pdo) < $_SESSION['total_places']) {
+				echo "<p>On est en 5</p>";
 				$query=$pdo->prepare("INSERT into nanovni (prenom,nom, email, heure) VALUES (?,?,?,?)");
 				$query->execute(array($_SESSION["prenom"],$_SESSION["nom"],$_SESSION["email"],$_SESSION['sg_time']));
 				//$_SESSION['shotgun'] = true;
 				header("refresh:5; url=/nanovni/index.php");
             } else {
+				echo "<p>On est en 6</p>";
 				header("refresh:5; url=/nanovni/fini.php");
             }
      	}
+		echo "<p>On est en 7</p>";
 	}
 } else if($_SESSION['shotgun']){
+	echo "<p>On est en 8</p>";
 	$sql ='DELETE from nanovni WHERE email=:email';
 	$stmt = $pdo->prepare($sql);
 	$stmt->bindValue(':email', $_SESSION["email"]);
@@ -70,6 +79,7 @@ if ($current_date_sec >= $shotgun_date_sec && $current_date_sec <= $end_date_sec
 	$_SESSION['shotgun'] = false;
 	header("Location: /nanovni/index.php");
 } else {
+	echo "<p>On est en 9</p>";
 	header("Location: /nanovni/");
 }
  ?>
