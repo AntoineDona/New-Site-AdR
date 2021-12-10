05fa17989f068f2f9094f11de21bb33bc3b51c40
Antoine Do Nascimento <antoinedn@live.fr>
2021-12-10 18:51:29 +0100

    feat: prise en charge variable env

--- latinano/auth.php
+++ latinano/auth.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+require ('../vendor/autoload.php');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
 if (!isset($_SESSION["isConnected"]) || !$_SESSION["isConnected"]) // Si isConnected pas défini ou faux
     
 {
