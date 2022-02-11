9a8472023b7c8bc5b4a469d0634ae4b7dd40b3ad
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-02-11 11:16:37 +0100

    feat: lien du sg dans la page event et place à 400

--- events/index.php
+++ events/index.php
@@ -17,6 +17,16 @@
             </a> -->
             <div class="container">
                 <h1 class="headline"> Nos Events</h1>
+                <div class='description_event'>
+                    <div ><img src="img/prison.png" alt="icon prison"><h2>Guantanano</h2><img src="img/prison.png" alt="icon prison"></div>
+                    <p>🎇 Les festivités sont de retour sur le campus 🎆
+                    La fin des restrictions sanitaires sonne le retour du rythme effréné des soirées, la vie reprend ! 🥳
+                    Libérez-vous donc dès le 16 Février et rejoignez-nous en Sd.001 pour fêter ça au Guantanano de 23h à 3h.🕊️
+                    Liberez la Grim 🍻 et les danses enflammées
+                    Shotgun pour les cotisants AdR le vendredi 11 Février à 13h.
+                    N'oublie pas ton pass sanitaire pour festoyer comme il se doit !💃</br>
+                    Le lien du shotgun : <a href="https://adr.cs-campus.fr/guantanano/">ici</a></p>
+                </div>
             </div>
         </section>
     </main>
