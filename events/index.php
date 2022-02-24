91f6eb5d0fe10f39416ec043f50bc8f88a49ce8e
Antoine Do Nascimento <antoinedn@live.fr>
2022-02-24 18:04:06 +0100

    fix: events style

--- events/index.php
+++ events/index.php
@@ -18,6 +18,31 @@
             <div class="container">
                 <h1 class="headline"> Nos Events</h1>
             </div>
+            <a href="/christmasbang/" id="lien_resa">Lien du Christmas Bang</a>
+        </section>
+        <section class="to_come">
+            <h2>Evenements à venir</h2>
+            <div class="event">
+                <div class="background" id="christmasbang"></div>
+                <div class="content">
+                    <div class="event_img">
+                    </div>
+                    <div class="infos">
+                        <h3>Christmas Bang</h3>
+                        <h4 class="date">Vendredi 17 décembre</h4>
+                        <p>❄️All I want for Christmas is a Bang❄️<br/>
+                        🎅🏻Le père Noël existe, et cette année il sera à Gif le 17 décembre🎅🏻</p>
+                        <a href="/christmasbang/">lien du site</a>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <section class="old_events">
+            <h2>Evenements passés</h2>
+            <div class="event">
+                
+            </div>
         </section>
     </main>
     <?php include "../included/footer.php" ?>
