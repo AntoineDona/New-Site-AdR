ad1cc6de8489921994f0ce65e3e08825aee5c5a0
AinaR91 <48097659+AinaR91@users.noreply.github.com>
2022-03-24 17:19:42 +0100

    Update index.php

--- papybang/index.php
+++ papybang/index.php
@@ -1,170 +1,168 @@
-<?php include("auth.php"); ?>
-<?php include("database.php"); ?>
+<?php
+session_start();
 
-<!DOCTYPE html><html>
+include("auth.php"); 
+include("database.php");
+// //
+// 	//
+// 	$_SESSION["isConnected"] = true;
+// 	$_SESSION["is_cotisant"]=false;
+// 	$_SESSION['shotgun']=true;
+// 	$_SESSION["user"]=array(
+// 		'firstName'=>'Bastien',
+// 		'lastName'=>'de Rugy',
+// 		'email'=>'B.gdr@student-cs.fr',
+// 		'promo'=>'2024',
+// 	);
+
+// 	//
+// 	//
+
+function number_place($pdo)
+{
+	// Checks the number of places still available to shotgun
+	$query = $pdo->prepare("SELECT COUNT(*) as c from guantanano");
+	$query->execute();
+	$result = $query->fetch();
+	return $result['c'];
+}
+
+
+function has__already_shotgun($email, $pdo)
+{
+	//  Checks if email already shotgun, return 1 if true and 0 else
+	$query = $pdo->prepare("SELECT COUNT(*) as c from guantanano where email=?");
+	$query->execute(array($email));
+	$result = $query->fetch();
+	if ($result['c'] == 0) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+
+function is_cotisant($email, $pdo)
+{
+	$query = $pdo->prepare("SELECT COUNT(*) as c from cotisants where email=?");
+	$query->execute(array($email));
+	$result = $query->fetch();
+	if ($result['c'] == 0) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+$_SESSION["prenom"] = $_SESSION["user"]["firstName"];
+$_SESSION["nom"] = $_SESSION["user"]["lastName"];
+$_SESSION["email"] = $_SESSION["user"]["email"];
+
+$_SESSION['total_places'] = 400;
+$_SESSION['shotgun'] = has__already_shotgun($_SESSION["email"], $pdo);
+$_SESSION["is_cotisant"] = is_cotisant($_SESSION["email"], $pdo);
+
+if ($_SESSION["email"] == '???') {
+	header("Location: /guantanano/troll.php");
+}
+
+// if (isset($_SESSION["preshotgun"]) && $_SESSION["preshotgun"]) {
+// 	// Si dans la liste de préshotgun -> shotgun direct, en vrai pas super utile...
+// 	$url = "https://adr.cs-campus.fr/guantanano/action.php";
+// 	header("Location: ".$url);
+//   }
+
+if (number_place($pdo) >= $_SESSION['total_places'] and !$_SESSION['shotgun']) {
+	if ($_SESSION["isConnected"]) {
+		header("Location: /guantanano/fini.php");
+	} else {
+		header("Location: /guantanano/connexion.php");
+		// header("Location: https://adr.cs-campus.fr
+	}
+}
+
+?>
+
+<!DOCTYPE html>
 <html>
+
 <head>
-<meta content="text/html; charset=utf-8" http-equiv="content-type">
-<title>EQUINANOXE</title><link rel="icon" type="image/png" href="img/adr_ico.png" />
-<link rel="stylesheet" type="text/css" href="styles.css" />
+	<meta content="text/html; charset=utf-8" http-equiv="content-type">
+	<title>GUANTANANO</title>
+	<meta name="google-site-verification" content="cEbrs-eyoHMLzEcQwiEu5sHkC8N61J92Z_fElR1KTMQ" />
+	<!-- <meta property="og:image" content="https://adr.cs-campus.fr/guantanano/img/fb_banner.jpg" /> -->
+	<link rel="shortcut icon" sizes="96x96" type="image/png" href="/guantanano/img/prison.png">
+	<link rel="stylesheet" type="text/css" href="styles.css"/>
 </head>
 
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-<?php include("script.js.php"); ?>
-
 <body onload="onLoad()">
-	
-	<!-- <a href="deconnexion.php">se deconnecter</a> -->
-	
-<?php
-function number_place($pdo){
-		$query = $pdo->prepare("SELECT COUNT(*) as c from equinanox");
-		$query->execute();
-		$result= $query->fetch();
-		return $result['c'];
-}
-?>
-	
-<?php
-	function is_present($prenom,$nom,$pdo){
-		$query = $pdo->prepare("SELECT COUNT(*) as c from equinanox where prenom=? and nom=?");
-		$query->execute(array($prenom,$nom));
-		$result= $query->fetch();
-		return $result['c'];
-	}
-	
-	
-	function is_cotiz($mail,$pdo){
-		$query = $pdo->prepare("SELECT COUNT(*) as c from cotisants_p2022 where EMAIL=? and COTISANT='oui'");
-		$query->execute(array($mail));
-		$result= $query->fetch();
-		return $result['c'];
-	}
+	<div id="titre_sg"></div>
+	<div id="counter">
+		<!-- <div class="digit_holder months" id="mois">
+			<p class="chiffre">00</p>
+			<p class="text">mois(s)</p>
+		</div> -->
+		<div class="digit_holder days" id="jours">
+			<p class="chiffre">00</p>
+			<p class="text">jours</p>
+		</div>
+		<div class="digit_holder hours" id="heures">
+			<p class="chiffre">00</p>
+			<p class="text">heures</p>
+		</div>
+		<div class="digit_holder minutes" id="minutes">
+			<p class="chiffre">00</p>
+			<p class="text">minutes</p>
+		</div>
+		<div class="digit_holder seconds" id="secondes">
+			<p class="chiffre">00</p>
+			<p class="text">secondes</p>
+		</div>
+	</div>
+	</div>
 
-?>
-	
-	
- <?php
-	$_SESSION["promo"]=$_SESSION["user"]["promo"];
-	$_SESSION["prenom"] = $_SESSION["user"]["firstName"];
-	$_SESSION["nom"] = $_SESSION["user"]["lastName"];
-	$_SESSION["mail"] = $_SESSION["user"]["email"];
-	
-	
-	
-	
-		//$req='select count(*) from nanoween where prenom=$_SESSION["prenom"] AND nom =$_SESSION["nom"]';
-		//$res=$pdo->query($req);
-	
-		$res = is_present($_SESSION["prenom"],$_SESSION["nom"],$pdo);
-		$cotiz = is_cotiz($_SESSION["mail"],$pdo);
-		
-		//echo $res;
-		//echo 'est ce que ça fonctionne vraiment?';
-		$reste=550-number_place($pdo);
-		//echo 'Il reste '.$reste.' places au shotgun';
-		if($res==0)
-		{
-			$_SESSION['shotgun'] = false;
-				//echo 'y a pas';
-			// la valeur n'existe pas -> action appropriée
-		}
-		else
-		{
-			$_SESSION['shotgun'] = true;
-			     //echo 'y a de ouf';
-		// valeur existe -> action appropriée
-		}
-	?>
-	
-	<?php 
-		if ($_SESSION["mail"]=='agathe.auburtin@student-cs.fr'){
-			header("Location: https://adr.cs-campus.fr/nano/agathe.php");
-		}
-	?>
-	
 	<?php
 	
-		if ($_SESSION["promo"]==2022){
-			if ($cotiz==0){
-				$_SESSION["cotisant"] = false;
-				header("Location: https://adr.cs-campus.fr/nano/non_cotisant.php");
-			}
-			else{
-				$_SESSION["cotisant"] = true;
-				//echo "trop cool tu as cotisé";
-			}
+	if (!$_SESSION['shotgun']) {
+		if (!$_SESSION["is_cotisant"]) {
+			echo "<p id='danger_msg_ctnr'>ATTENTION !! <br/> Tu n'est pas cotisant, tu NE POURRAS PAS SHOTGUN!!</p>";
+		} else {
+			echo "<p id='ok_msg_ctnr'> Salut " . $_SESSION["prenom"] . "! <br> Tu es bien cotisant, tu vas pouvoir Shotgun! </p>";
 		}
-		else{
-			$_SESSION["cotisant"] = true;
-			//echo "bah toi t'es en 2A ou plus";
+	} else {
+		echo "<p id='ok_msg_ctnr'> Bravo " . $_SESSION["prenom"] . "! <br> Tu as réussi à shotgun ta place au guantanano. </p>";
+	}
+	?>
+	<div id="sg_link_ctnr">
+		<?php
+		if (!$_SESSION['shotgun']) {
+			echo '<a id="sg_link" href="action.php">SHOTGUN</a>';
+		} else {
+			echo '<a id="sg_link" href=# onClick="verif();">DEPAPS</a>';
 		}
-	
-      //echo 'bonjour'; ?>
+		?>
+	</div>
+	<p id="footer">Pas besoin de recharger la page au moment du shotgun!</p>
 
-	
-<?php
-if (number_place($pdo)>=550 AND !$_SESSION['shotgun']){
-	if ($_SESSION["isConnected"]){
-		header("Location: https://adr.cs-campus.fr/papybang/fini.php");
+	<?php include("script.php"); 
+	if (!$_SESSION["is_cotisant"] && isset($_SESSION['prev_page']) && $_SESSION['prev_page'] == "action.php") {
+		// Pour ceux qui ont cliqué sur shotgun et qui ne sont pas cotisants
+		echo "<script>alert(\"Tu n'es pas cotisant desolé... \")</script>";
+		$_SESSION['prev_page'] = "index.php";
 	}
-	else{
-		header("Location: https://adr.cs-campus.fr/papybang/connexion.php");
-	}
-}
-	
-?>
-	
-	<div id="holder">
-		<div id="counter">
-			<div style="grid-area: d_top;">01</div>
-			<div style="grid-area: h_top;">02</div>
-			<div style="grid-area: m_top;">03</div>
-			<div style="grid-area: s_top;">04</div>
-			<div style="grid-area: d_bottom; font-size:20%;">jour(s)</div>
-			<div style="grid-area: h_bottom; font-size:20%;">heure(s)</div>
-			<div style="grid-area: m_bottom; font-size:20%;">minute(s)</div>
-			<div style="grid-area: s_bottom; font-size:20%;">seconde(s)</div>
-		</div>
-		
-		<script type="text/javascript">
-			function shotgun() {
-				window.location='action.php';
+	?>
+	<script type="text/javascript">
+		function shotgun() {
+			window.location = 'action.php';
+		}
+
+		function verif() {
+			if (confirm("ATTENTION: En cliquant sur OK, tu annules ta place et la remets en jeu.")) {
+				window.location = 'action.php';
 			}
-			function verif() {
-				if ( confirm( "En cliquant sur OK, tu annules ta place et la remets en jeu." ) ) {
-					window.location='action.php';
-				}
-			}
-		</script>
-		
-		<div id="link" href="#">
-			<a class="shotgun" href="#">
-				<?php
-				if (!$_SESSION['shotgun']) {
-					echo '<p id="shotgun" onClick="shotgun();">SHOTGUN</p>';
-				}
-				else {
-					echo '<p id="shotgun" onClick="verif();">DEPAPS</p>';
-				}
-				?>
-			</a>
-		</div>
-	</div>
-	
-	<div class="etat">
-		<p>Salut <?php echo $_SESSION["prenom"].' '.$_SESSION["nom"]; ?> !<br> 
-		<?php 
-			if ($_SESSION['shotgun']) {
-				echo 'Tu as déjà ta place !';
-				
-			}
-			else {
-				echo "Tu n'as pas encore ta place !";
-			}
-		?>  
-		</p>
-	</div>
+		}
+	</script>
 
 </body>
+
 </html>
