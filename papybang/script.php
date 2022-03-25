ddfea031b7bc19a72a8df49ba24c49752ea2596e
AinaR91 <48097659+AinaR91@users.noreply.github.com>
2022-03-25 09:44:23 +0100

    Update script.php

--- papybang/script.php
+++ papybang/script.php
@@ -24,7 +24,7 @@
 		var date = new Date();
 		// console.log(date);
 		<?php //$_SESSION['shotgun'] = false; ?>
-		var date_shotgun = new Date(2022, 02, 24, 13, 00, 00);
+		var date_shotgun = new Date(2022, 02, 25, 13, 00, 00);
 		var date_soiree = new Date(2022, 03, 01, 23, 00);
 		var title = "Shotgun dans :";
 		var total_sec = Math.trunc((date_shotgun.getTime() - date.getTime()) / 1000);
