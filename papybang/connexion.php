bde11bbb79b331bcd2b640a27aa52cf5a25e2f71
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-03-27 23:32:11 +0200

    fix:css sg

--- papybang/connexion.php
+++ papybang/connexion.php
@@ -33,43 +33,45 @@ if (isset($_SESSION["isConnected"]) && $_SESSION["isConnected"]) {
 	<div id="logobang">
 		<img class="logobang_img" src="img/proj4_title.png">
 	</div>
-	<div id="tease">
+	<!-- <div id="tease">
 	<p>Rejoins nous pour une dernière soirée, à l'ancienne, parce que c'était mieux avant. 
 	J'espère que ce petit site rudimentaire fait remonter des souvenirs de doute et de stress.</p>
-	</div>
-	<div id="titre_sg" style='margin-top: 40vh;' >Le shotgun commence dans:</div>
-	<div id="counter">
-		<!-- <div class="digit_holder months" id="mois">
-			<p class="chiffre">00</p>
-			<p class="text">mois(s)</p>
-		</div> -->
-		<div class="digit_holder days" id="jours">
-			<p class="chiffre">00</p>
-			<p class="text">jours</p>
+	</div> -->
+	<div id='sgSpace'>
+		<div id="titre_sg" >Le shotgun <br>commence dans:</div>
+		<div id="counter">
+			<!-- <div class="digit_holder months" id="mois">
+				<p class="chiffre">00</p>
+				<p class="text">mois(s)</p>
+			</div> -->
+			<!-- <div class="digit_holder days" id="jours">
+				<p class="chiffre">00</p>
+				<p class="text">jours</p>
+			</div> -->
+			<div class="digit_holder hours" id="heures">
+				<p class="chiffre">00</p>
+				<p class="text">heures</p>
+			</div>
+			<div class="digit_holder minutes" id="minutes">
+				<p class="chiffre">00</p>
+				<p class="text">minutes</p>
+			</div>
+			<div class="digit_holder seconds" id="secondes">
+				<p class="chiffre">00</p>
+				<p class="text">secondes</p>
+			</div>
 		</div>
-		<div class="digit_holder hours" id="heures">
-			<p class="chiffre">00</p>
-			<p class="text">heures</p>
-		</div>
-		<div class="digit_holder minutes" id="minutes">
-			<p class="chiffre">00</p>
-			<p class="text">minutes</p>
-		</div>
-		<div class="digit_holder seconds" id="secondes">
-			<p class="chiffre">00</p>
-			<p class="text">secondes</p>
-		</div>
-	</div>
-		<!-- <?php
-		if (isset($_SESSION["isConnected"])) {
-			if (!$_SESSION["isConnected"]) {
-				echo "<p id='co_msg_ctnr'>Tu n'es pas encore connecté</p> ";
+			<!-- <?php
+			if (isset($_SESSION["isConnected"])) {
+				if (!$_SESSION["isConnected"]) {
+					echo "<p id='co_msg_ctnr'>Tu n'es pas encore connecté</p> ";
+				}
 			}
-		}
-		?> -->
+			?> -->
 	<div id="co_link_ctnr" href="#">
 		<a id="co_link" href='redirect.php?redirect=https://adr.cs-campus.fr/papybang'>Se connecter</a>
 	</div>
+</div>
 	<?php include("script.php"); ?>
 </body>
 
