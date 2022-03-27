2ffa6148a47416c55baa5e3202804a833992c22d
AinaR91 <48097659+AinaR91@users.noreply.github.com>
2022-03-27 10:39:16 +0200

    Update connexion.php

--- papybang/connexion.php
+++ papybang/connexion.php
@@ -30,6 +30,9 @@ if (isset($_SESSION["isConnected"]) && $_SESSION["isConnected"]) {
 		echo "<script>alert(\"Tu as bien été déconnecté \")</script>";
 	}
 	?>
+	<div id="logobang">
+		<img src="proj4_title.png">
+	</div>
 	<div id="tease">
 	<p>Rejoins nous pour une dernière soirée, à lancienne, parce que c'était mieux avant. 
 	J'espère que ce petit site rudimentaire fait remonter des souvenirs de doute et de stress.</p>
