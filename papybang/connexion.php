aeae3ec4a682c4d353304cdb6f6b975e5d89ab7f
AinaR91 <48097659+AinaR91@users.noreply.github.com>
2022-03-27 10:11:43 +0200

    Update connexion.php

--- papybang/connexion.php
+++ papybang/connexion.php
@@ -34,7 +34,7 @@ if (isset($_SESSION["isConnected"]) && $_SESSION["isConnected"]) {
 	<p>Rejoins nous pour une dernière soirée, à lancienne, parce que c'était mieux avant. 
 	J'espère que ce petit site rudimentaire fait remonter des souvenirs de doute et de stress.</p>
 	</div>
-	<div id="titre_sg" style='margin-top: 60vh;' >Le shotgun commence dans:</div>
+	<div id="titre_sg" style='margin-top: 40vh;' >Le shotgun commence dans:</div>
 	<div id="counter">
 		<!-- <div class="digit_holder months" id="mois">
 			<p class="chiffre">00</p>
