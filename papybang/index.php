a3e341b9ae8782d083df095cd10aa6ae6e1cf24b
AinaR91 <48097659+AinaR91@users.noreply.github.com>
2022-03-27 20:20:03 +0200

    Update index.php

--- papybang/index.php
+++ papybang/index.php
@@ -147,8 +147,8 @@ if (number_place($pdo) >= $_SESSION['total_places'] and !$_SESSION['shotgun']) {
 	}
 	?>
 	
-	<div id="titre_sg" class="titre_index" style='top: 10vh;'></div>
-	<div id="counter" class="counter_index" style='top: 10vh;'>
+	<div id="titre_sg" class="titre_index" style='top: -50vh;'></div>
+	<div id="counter" class="counter_index" style='top: -50vh;'>
 		<!-- <div class="digit_holder months" id="mois">
 			<p class="chiffre">00</p>
 			<p class="text">mois(s)</p>
