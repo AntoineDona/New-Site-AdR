392c248c56f16f53ff9854c66c0e200c28bb897f
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-03-28 13:13:36 +0200

    fix: bug sg

--- papybang/action.php
+++ papybang/action.php
@@ -82,7 +82,7 @@ include('database.php') ?>
 	$end_date = mktime(03, 00, 00, 04, 02, 2022);
 	$end_date_sec = (((date('d', $end_date) - 1) * 24 + date('H', $end_date)) * 60 + date('i', $end_date)) * 60 + date('s', $end_date);
 	if ($current_date_sec >= $shotgun_date_sec && $current_date_sec <= $end_date_sec) {
-		if(isOld($_SESSION["login"]) or (isYoung($_SESSION["login"]) and $current_date_sec >= $shotgun_date_young_sec)){#ligne pour la condition sur les vieux
+		if(isOld($_SESSION["login"])){#ligne pour la condition sur les vieux
 			if ($_SESSION["is_cotisant"] == false) {
 				$_SESSION['prev_page'] = "action.php";
 				header("Location: /papybang/index.php");
