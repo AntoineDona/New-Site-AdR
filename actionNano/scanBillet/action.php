8a75ccd7f3ca000eb33ea39bf97003f55db1b419
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-03-31 20:19:41 +0200

    feat:système de qr code

--- actionNano/scanBillet/action.php
+++ actionNano/scanBillet/action.php
@@ -7,20 +7,25 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../styles.css" />
     <title>SCAN NANO</title>
 </head>
-<body>
-    
+<body class='align'>
+    <div class="grid">
+        <div class="form_field">
+            <?php
+            $password=$_POST['password'];
+            if($password!='1969'){
+                header('Location: index.php');
+                $_SESSION['error']=true;
+            }
+            else{
+                $_SESSION['is_connected']=true;
+                echo '<p class="message">Tu es connecté(e), tu peux commencer à scanner les billets.</p>';
+            }
+            ?>
+        </div>  
+    </div>
 </body>
+
 </html>
-<?php
-$password=$_POST['code'];
-if($password!='1969'){
-    header('Location: index.php');
-    $_SESSION['error']=true;
-}
-else{
-    $_SESSION['is_connected']=true;
-    echo '<p>Tu es connecté(e), tu peux commencer à scanner les billets.</p>';
-}
-?>
