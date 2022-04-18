791da103dbe784ba41a661b0ba41eb2d09283ef1
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-04-18 13:14:29 +0200

    feat:sg vieux

---
+++ actionNano/index.php
@@ -0,0 +1,30 @@
+<?php
+// fichiers pour ajouter les vieux qui se sont listés
+session_start();
+
+//indiquer ci-dessous le nom de db du nano associer
+$_SESSION['nomNano']='bronzenano';
+
+include("auth.php"); 
+include("database.php");
+
+$_SESSION["prenom"] = $_SESSION["user"]["firstName"];
+$_SESSION["nom"] = $_SESSION["user"]["lastName"];
+$_SESSION["email"] = $_SESSION["user"]["email"];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../styles.css" />
+    <title>SG VIEUX</title>
+</head>
+<body>
+    <p>Connecté</p>
+</body>
+</html>
