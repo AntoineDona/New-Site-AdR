c5e688c17e278b468c5897de961fa3d25eba332f
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-04-18 17:07:47 +0200

    version finale sg vieux

--- actionNano/action.php
+++ actionNano/action.php
@@ -2,26 +2,16 @@
 session_start();
 include("auth.php"); 
 include("database.php");
+$_SESSION['sg_time'] = date("Y-m-d H:i:s");
 
-function depaps($email, $pdo)
-	{
-		$sql = 'DELETE from bronzenano WHERE email=:email';
-		$stmt = $pdo->prepare($sql);
-		$stmt->bindValue(':email', $email);
-		$res = $stmt->execute();
-		$_SESSION['shotgun'] = false;
-		header("Location: /actionNano/index.php");
-	}
-
-if (!$_SESSION['shotgun']) {
-
-    $query = $pdo->prepare("INSERT into ? (prenom,nom, email, heure) VALUES (?,?,?,?)");
-    $query->execute(array($_SESSION['nomNano'],$_SESSION["prenom"], $_SESSION["nom"], $_SESSION["email"], $_SESSION['sg_time']));
-    $_SESSION['shotgun'] = true;
-    header("refresh:5; url=/actionNano/index.php");
-} 
-else {
-    depaps($_SESSION["email"], $pdo);
-}
+$query = $pdo->prepare("INSERT INTO ".$_SESSION['nomNano']."(prenom,nom,email,heure) VALUES (:prenom,:nom,:email,:heure)");
+$query->execute([
+	'prenom'=>$_SESSION["prenom"], 
+	'nom'=>$_SESSION["nom"], 
+	'email'=>$_SESSION["email"],
+	'heure'=>$_SESSION['sg_time'],
+]);
+$_SESSION['shotgun'] = true;
+header('Location:../'.$_SESSION['nomNano']);
 
 ?>
