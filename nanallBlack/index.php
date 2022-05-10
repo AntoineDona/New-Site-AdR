c3f985e48df8874e806f22be98e92b6c6cf875d8
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-05-10 14:25:04 +0200

    ajout page nanall black

--- nanallBlack/index.php
+++ nanallBlack/index.php
@@ -5,16 +5,16 @@ include("auth.php");
 include("database.php");
 // //
 // 	//
-	$_SESSION["isConnected"] = true;
-	$_SESSION["is_cotisant"]=true;
-	// $_SESSION['shotgun']=false;
-	$_SESSION['login']='2021goulletba';
-	$_SESSION["user"]=array(
-		'firstName'=>'Bastien',
-		'lastName'=>'de Rugy',
-		'email'=>'bastien.goullet-de-rugy@student-cs.fr',
-		'promo'=>'2024',
-	);
+	// $_SESSION["isConnected"] = true;
+	// $_SESSION["is_cotisant"]=true;
+	// // $_SESSION['shotgun']=false;
+	// $_SESSION['login']='2021goulletba';
+	// $_SESSION["user"]=array(
+	// 	'firstName'=>'Bastien',
+	// 	'lastName'=>'de Rugy',
+	// 	'email'=>'bastien.goullet-de-rugy@student-cs.fr',
+	// 	'promo'=>'2024',
+	// );
 
 // 	//
 // 	//
