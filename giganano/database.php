a694354d305df64629e9be48350a807379182a94
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-10-13 16:24:20 +0200

    sans mamp

--- giganano/database.php
+++ giganano/database.php
@@ -7,6 +7,6 @@ $DBName  = "adr";
 $DBconnect = "mysql:dbname=".$DBName.";host=".$DBhost;
 
 // $pdo = new PDO($DBconnect, $DBowner, $DBpw);
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'root', 'root');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'bastien', 'baba92');
 
 ?>
