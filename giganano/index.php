e3c62a309ed85e3c55b0a6c20d05bf9c46fb95a4
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-10-13 16:25:42 +0200

    sans mamp

--- giganano/index.php
+++ giganano/index.php
@@ -55,23 +55,21 @@ function is_cotisant($email, $pdo)
 	}
 }
 
-// $_SESSION["prenom"] = $_SESSION["user"]["firstName"];
-// $_SESSION["nom"] = $_SESSION["user"]["lastName"];
-// $_SESSION["email"] = $_SESSION["user"]["email"];
-// if($_SESSION['user']['login']=='2021goulletba'){
-// 	$_SESSION['promo']='2024';
-// }
-// else{
-// $_SESSION['promo']=$_SESSION['user']['promo'];
-// }
+$_SESSION["prenom"] = $_SESSION["user"]["firstName"];
+$_SESSION["nom"] = $_SESSION["user"]["lastName"];
+$_SESSION["email"] = $_SESSION["user"]["email"];
+if($_SESSION['user']['login']=='2021goulletba'){
+	$_SESSION['promo']='2024';
+}
+else{
+$_SESSION['promo']=$_SESSION['user']['promo'];
+}
 
 $_SESSION['total_places'] = 700;
 // $_SESSION['shotgun'] = has__already_shotgun($_SESSION["email"], $pdo);
 // $_SESSION["is_cotisant"] = is_cotisant($_SESSION["email"], $pdo);
 
-if ($_SESSION["email"] == '???') {
-	header("Location: /giganano/troll.php");
-}
+
 
 // if (isset($_SESSION["preshotgun"]) && $_SESSION["preshotgun"]) {
 // 	// Si dans la liste de préshotgun -> shotgun direct, en vrai pas super utile...
