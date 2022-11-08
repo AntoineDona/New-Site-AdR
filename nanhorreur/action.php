303d9cbb8e5f4b5d2a0e12eb09f6c54b34fc0584
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-11-08 13:03:23 +0100

    version finale

--- nanhorreur/action.php
+++ nanhorreur/action.php
@@ -53,7 +53,7 @@ include('database.php') ?>
 	date_default_timezone_set('Europe/Paris');
 	$current_date_sec = (((date('d') - 1) * 24 + date('H')) * 60 + date('i')) * 60 + date('s');
 
-	$shotgun_date = mktime(12, 59, 59, 11, 08, 2022);
+	$shotgun_date = mktime(13, 04, 59, 11, 08, 2022);
 	$shotgun_date_sec = (((date('d', $shotgun_date) - 1) * 24 + date('H', $shotgun_date)) * 60 + date('i', $shotgun_date)) * 60 + date('s', $shotgun_date);
 
 	$end_date = mktime(03, 00, 0, 11, 10, 2022);
