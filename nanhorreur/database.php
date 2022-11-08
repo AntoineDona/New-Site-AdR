873d0cb06f2ed5c69bfce7fd159199cedef0c8c6
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-11-08 01:33:58 +0100

    test version

--- nanhorreur/database.php
+++ nanhorreur/database.php
@@ -6,7 +6,8 @@ $DBpw    = "root";
 $DBName  = "adr";
 $DBconnect = "mysql:dbname=".$DBName.";host=".$DBhost;
 
-$pdo = new PDO($DBconnect, $DBowner, $DBpw);
+// $pdo = new PDO($DBconnect, $DBowner, $DBpw);
 // $pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'root', 'root');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'bastien', 'baba92');
 
 ?>
