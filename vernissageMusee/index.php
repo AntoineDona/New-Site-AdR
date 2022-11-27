d4398275df79ac0084a97677894598ae029abbfe
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-11-27 19:38:03 +0100

    vernissage musée

--- vernissageMusee/index.php
+++ vernissageMusee/index.php
@@ -1,1 +1,28 @@
-<p>site en construction ...</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" />
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">    
+    <div class="text">
+        <div class="text-inner">
+          <h1> Vernissage au Musée </h1>
+      
+          <h2> Mercredi 30 novembre aura lieu le Vernissage 
+            C’est un repas classe 🍽️ fait maison, au Musée, avec entrée, plat, dessert et vin🍷 , et tout ça servi à table dans une ambiance musicale de jazz 🎷, tout ça pour 13€.  </h2>
+      
+          
+        <a href="menu.html"><div class="button">MENU</div></a>
+        <a href="https://collecte.io/vernissage-musee-2182272/fr"><div class="button">BILLETTERIE</div></a>
+    </div>
+    
+</div>
+      
+      </div>
+</body>
+</html>
