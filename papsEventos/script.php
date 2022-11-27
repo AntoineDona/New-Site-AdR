d6aaf4fce679ece97443cf80b0e6e226e4020181
de Rugy Bastien <bastien.goullet-de-rugy@student-cs.fr>
2022-11-27 18:54:59 +0100

    link

--- papsEventos/script.php
+++ papsEventos/script.php
@@ -23,8 +23,8 @@
 	function timer() {
 		var date = new Date();
 		// console.log(date);
-		var date_shotgun = new Date(2022, 10, 21, 13, 00, 00);
-		var date_soiree = new Date(2022, 10, 22, 21, 00, 00);
+		var date_shotgun = new Date(2022, 10, 28, 13, 00, 00);
+		var date_soiree = new Date(2022, 10, 30, 19, 00, 00);
 		var title = "Shotgun dans :";
 		var total_sec = Math.trunc((date_shotgun.getTime() - date.getTime()) / 1000);
 		var has_shotgun = <?php echo json_encode(isset($_SESSION['shotgun']))?> && <?php echo json_encode($_SESSION['shotgun']) ?>;
