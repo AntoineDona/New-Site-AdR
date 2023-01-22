e84d66d8cbb2e0d56a1dd77a5d681bdc65eba79a
Tillet Marjolaine <marjolaine.tillet@student-cs.fr>
2023-01-22 23:34:55 +0100

    test

---
+++ marjotest/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$DBhost  = "localhost";
+$DBowner = "root";
+$DBpw    = "root";
+$DBName  = "adr";
+$DBconnect = "mysql:dbname=".$DBName.";host=".$DBhost;
+
+// $pdo = new PDO($DBconnect, $DBowner, $DBpw);
+//$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'root', 'root');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'marjolaine', 'marjo');
+
+?>
