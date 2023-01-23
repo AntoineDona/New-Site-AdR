5fb7e203f5fbea2483c89fc8a75c2bfa78d69a9d
Tillet Marjolaine <marjolaine.tillet@student-cs.fr>
2023-01-23 10:44:12 +0100

    astronano v3

--- astronano/connexion.php
+++ astronano/connexion.php
@@ -63,7 +63,7 @@ if (isset($_SESSION["isConnected"]) && $_SESSION["isConnected"]) {
 		}
 		?> -->
 	<div class="link_ctnr" href="#">
-		<a class="link" href='redirect.php?redirect=https://adr.cs-campus.fr/marjotest'>Se connecter</a>
+		<a class="link" href='redirect.php?redirect=https://adr.cs-campus.fr/astronano'>Se connecter</a>
 	</div>
 	</div>
 	<?php include("script.php"); ?>
