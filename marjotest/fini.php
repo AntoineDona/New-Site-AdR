afb4883cdf4e2e147aa5b7b82f535be242a934a7
marjolainett <marjolaine.tt@gmail.com>
2023-04-15 15:13:27 +0200

    test

---
+++ marjotest/fini.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+include("database.php"); 
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>ASTRONANO</title>
+	<link rel="shortcut icon" sizes="96x96" type="image/png" href="/marjotest/img/icon.png">
+	<link rel="stylesheet" href="styles.css">
+</head>
+
+<?php
+function number_place($pdo)
+{
+	$query = $pdo->prepare("SELECT COUNT(*) as c from indiananones");
+	$query->execute();
+	$result = $query->fetch();
+	return $result['c'];
+}
+if (number_place($pdo) < $_SESSION["total_places"]) {
+	if ($_SESSION["isConnected"]) {
+		header("Location: /marjotest/index.php");
+	} else {
+		header("Location: /marjotest/connexion.php");
+	}
+}
+if ($_SESSION['shotgun']) {
+	header("Location: /marjotest/index.php");
+}
+?>
+
+<body>
+	<div class="soldout_ctnr">
+		<h1 id="soldout_title">SOLD-OUT</h1>
+		<p> Désolé <?php echo $_SESSION["prenom"]; ?>, <br />
+			le monde appartient à ceux qui se lèvent tôt... <br />
+			Vérifie si tu as shotgun en cliquant sur recharger :
+		</p>
+		<div class="btns_ctnr">
+			<div href="#" onClick="window.location='index.php'" id="reload">&#8635; Recharger</div>
+			<div href="#" onClick="window.location='logout.php'" id="reload">Se déconnecter</div>
+		</div>
+	</div>
+
+</body>
+
+</html>
