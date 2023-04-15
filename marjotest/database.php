04fe794ed132800eb6a212f5fc9e70ff8aa93a06
marjolainett <marjolaine.tt@gmail.com>
2023-04-15 15:47:16 +0200

    test

--- marjotest/database.php
+++ marjotest/database.php
@@ -7,7 +7,7 @@ $DBName  = "adr";
 $DBconnect = "mysql:dbname=".$DBName.";host=".$DBhost;
 
 // $pdo = new PDO($DBconnect, $DBowner, $DBpw);
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'root', 'root');
-//$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'marjolaine', 'marjo');
+//$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'root', 'root');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adr;charset=utf8', 'marjolaine', 'marjo');
 
 ?>
